c0e22cfca8d5888a44c5867190a7d3e4bf137441
Kristofer Walters <kw@knowit.no>
2016-01-14 14:02:26 +0100

    Use relative path for fetch if code runs in browser

--- src/actions/authenticationSuccess.js
+++ src/actions/authenticationSuccess.js
@@ -2,10 +2,14 @@ import 'isomorphic-fetch';
 import { setUserData, setAuthToken } from '.';
 import { replacePath } from 'redux-simple-router';
 
+const url = (typeof window === 'undefined') ?
+  (path) => 'http://localhost:8080' + path :
+  (path) => path;
+
 export default function authenticationSuccess (authToken) {
   return dispatch => {
     dispatch(setAuthToken(authToken));
-    fetch('http://localhost:8080/auth/me', {
+    fetch( url('/auth/me'), {
       method: 'GET',
       headers: {'APP-KEY': authToken}
     })
