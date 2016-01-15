91c8e63b7534f3cd8c909d41bdecf2f6cbabe324
Kristofer Walters <kw@knowit.no>
2016-01-15 12:17:11 +0100

    Pass return path to login provider

--- src/components/LoginProviders.js
+++ src/components/LoginProviders.js
@@ -5,9 +5,9 @@ export default function LoginProviders() {
     <div>
       Logg inn i NDLA med
       <ul>
-        <li><a href='/auth/login/google'>Google</a></li>
-        <li><a href='/auth/login/facebook'>Facebook</a></li>
-        <li><a href='/auth/login/twitter'>Twitter</a></li>
+        <li><a href='/auth/login/google?successUrl=/learningpath/login/success/{appkey}&failureUrl=/learningpath/login/failure'>Google</a></li>
+        <li><a href='/auth/login/facebook?successUrl=/learningpath/login/success/{appkey}&failureUrl=/learningpath/login/failure'>Facebook</a></li>
+        <li><a href='/auth/login/twitter?successUrl=/learningpath/login/success/{appkey}&failureUrl=/learningpath/login/failure'>Twitter</a></li>
       </ul>
     </div>
   );
