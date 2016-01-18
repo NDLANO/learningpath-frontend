6a5e1d8cc19fc62d47e6fbd82cf7e01b4e7c57b4
Kristofer Walters <kw@knowit.no>
2016-01-18 12:26:16 +0100

    Test for AuthTokenSetter.

---
+++ src/components/AuthTokenSetter-test.js
@@ -0,0 +1,43 @@
+import test from 'tape';
+import React from 'react';
+import TestUtils from 'react-addons-test-utils';
+import sinon from 'sinon';
+import { replacePath } from 'redux-simple-router';
+
+import { AuthTokenSetter } from './AuthTokenSetter';
+
+function setup (props={}) {
+  const renderer = TestUtils.createRenderer();
+  renderer.render(<AuthTokenSetter {...props} />);
+  const output = renderer.getRenderOutput();
+  return { props, output, renderer };
+}
+
+
+test('component/AuthTokenSetter', t => {
+  const p = Promise.resolve();
+
+  const dispatch = sinon.spy(() => p);
+
+  const params = { authToken: '12345' };
+
+  setup({dispatch, params});
+
+  p.then(() => {
+    t.ok(dispatch.calledTwice);
+    t.equals(typeof dispatch.firstCall.args[0], 'function', 'authenticationSuccess thunk');
+    t.deepEquals(dispatch.secondCall.args[0], replacePath('/'));
+
+    t.end();
+  }).catch((reason) => t.fail(reason));
+});
+
+test('component/AuthTokenSetter without authToken', t => {
+  const dispatch = sinon.spy(() => Promise.resolve());
+
+  setup({dispatch, params: {}});
+
+  t.notOk(dispatch.called);
+  t.end();
+});
+
