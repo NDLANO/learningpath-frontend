cbf38bc449a519875c8a94559af6cf3cb131b062
Kristofer Walters <kw@knowit.no>
2016-02-12 11:32:41 +0100

    Update layout of login page

--- src/components/LoginProviders.js
+++ src/components/LoginProviders.js
@@ -6,12 +6,12 @@ const query = '?successUrl=' + locationOrigin + '/login/success/{appkey}' +
 
 export default function LoginProviders() {
   return (
-    <div id='login'>
+    <div className='one-column one-column--narrow'>
       <h3>Logg inn i NDLA med</h3>
-      <ul>
-        <li><a className='cta-link cta-link--gl' href={apiResourceUrl('/auth/login/google' + query)}>Google</a></li>
-        <li><a className='cta-link cta-link--fb' href={apiResourceUrl('/auth/login/facebook' + query)}>Facebook</a></li>
-        <li><a className='cta-link cta-link--tw' href={apiResourceUrl('/auth/login/twitter' + query)}>Twitter</a></li>
+      <ul className='vertical-menu'>
+        <li className='vertical-menu_item'><a className='cta-link cta-link--block cta-link--gl' href={apiResourceUrl('/auth/login/google' + query)}>Google</a></li>
+        <li className='vertical-menu_item'><a className='cta-link cta-link--block cta-link--fb' href={apiResourceUrl('/auth/login/facebook' + query)}>Facebook</a></li>
+        <li className='vertical-menu_item'><a className='cta-link cta-link--block cta-link--tw' href={apiResourceUrl('/auth/login/twitter' + query)}>Twitter</a></li>
       </ul>
     </div>
   );
