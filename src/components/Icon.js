9ea472e84f3a18feb96a0fa45bad40a68fe208ae
Kristofer Walters <kw@knowit.no>
2016-02-23 23:59:55 +0100

    Update html and css for edit learning path

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -15,5 +15,12 @@ Icon.propTypes = { svgDefId: PropTypes.string.isRequired };
 Icon.Person = props => (<Icon {...props} className={'icon--person '+props.className} svgDefId='icon-person' />);
 Icon.Today  = props => (<Icon {...props} className={'icon--today '+props.className} svgDefId='icon-today' />);
 Icon.QueryBuilder = props => (<Icon {...props} className={'icon--query-builder '+props.className} svgDefId='icon-query_builder' />);
+Icon.TypeText = props => (<Icon {...props} className={'icon--menu '+props.className} svgDefId='icon-menu' />);
+Icon.TypeMedia = props => (<Icon {...props} className={'icon--play-circle-outline '+props.className} svgDefId='icon-play_circle_outline' />);
+Icon.TypeQuiz = props => (<Icon {...props} className={'icon--contacts '+props.className} svgDefId='icon-contacts' />);
+Icon.TypeTask = Icon.TypeText;
+Icon.TypeSummary = Icon.TypeText;
+Icon.Save = props => (<Icon {...props} className={'icon--save '+props.className} svgDefId='icon-save' />);
+Icon.Add = props => (<Icon {...props} className={'icon--add-circle-outline '+props.className} svgDefId='icon-add_circle_outline' />);
 
 export default Icon;
