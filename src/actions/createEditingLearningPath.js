462b5dd36afdfc523913d3e58206df7e2156ad0b
Kristofer Walters <kw@knowit.no>
2016-03-10 13:40:10 +0100

    bugfix: create learning path did not send data to api
    A missing parameter caused an empty body to be posted to /learningpaths,
    which replied with validation errors.

--- src/actions/createEditingLearningPath.js
+++ src/actions/createEditingLearningPath.js
@@ -2,7 +2,7 @@ import { setEditingLearningPath, applicationError } from '.';
 import { createPrivatePath } from '../sources/learningpaths';
 
 export default function createEditingLearningPath (learningPath) {
-  return (dispatch, getState) => createPrivatePath(getState().authToken, learningPath)
+  return (dispatch, getState) => createPrivatePath(getState().authToken, {}, learningPath)
     .then(lpath => dispatch(setEditingLearningPath(lpath)))
     .catch(err => dispatch(applicationError(err)));
 }
--- src/actions/createEmptyEditingPath.js
+++ src/actions/createEmptyEditingPath.js
@@ -3,6 +3,7 @@ import { setEditingLearningPath } from '.';
 export default function createEmptyEditingPath () {
   return setEditingLearningPath({
     title: [],
+    description: [],
     learningsteps: []
   });
 }
