d7335a2c6ad13de8d4084752d9ef257e46e5a13c
Kristofer Walters <kw@knowit.no>
2016-03-10 10:45:26 +0100

    Use shared SiteNav in default page container component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import Masthead from './Masthead';
+
+import Logo from './Logo';
+import SiteNav from './SiteNav';
 
 export function App({children}) {
   return (
     <div>
-      <Masthead />
+      <div className='masthead'>
+        <div className='masthead_left'>
+          <Logo />
+        </div>
+        <div className='masthead_right'>
+          <SiteNav />
+        </div>
+      </div>
       <div className='content'>
         {children}
       </div>
