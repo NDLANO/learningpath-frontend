568211e354284a8aa6da749687f40bbd065a22f1
Kristofer Walters <kw@knowit.no>
2016-03-21 22:51:12 +0100

    Update eslint configuration and some documentation

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,14 +4,17 @@ module.exports = {
     'quotes': [ 2, 'single' ],
     'linebreak-style': [ 2, 'unix' ],
     'semi': [ 2, 'always' ],
-    'no-unused-vars': [ 1 ]
+    'no-unused-vars': [ 1 ],
+    'react/display-name': [ 0 ],
+    'react/no-danger': [ 1 ],
+    'react/prop-types': [ 2, {'ignore': ['children','className'] }]
   },
   'env': {
     'es6': true,
     'node': true,
     'browser': true
   },
-  'extends': 'eslint:recommended',
+  'extends': ['eslint:recommended', 'plugin:react/recommended'],
   'parser': 'babel-eslint',
   'parserOptions': {
     'sourceType': 'module',
