8c7f23f6b88a202c598d3702f733e3433ca42123
Kristofer Walters <kw@knowit.no>
2016-03-22 22:48:48 +0100

    Test for actions/logout

---
+++ src/actions/__tests__/logout-test.js
@@ -0,0 +1,60 @@
+import test from 'tape';
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import nock from 'nock';
+import payload403invalid from './payload403invalid';
+
+import actions from '..';
+
+const middleware = [ thunk ];
+const mockStore = configureStore(middleware);
+
+const authToken = '123345';
+
+test('actions/logout', t => {
+  const done = res => {
+    t.end(res);
+    nock.cleanAll();
+  };
+
+  const apiMock = nock('http://ndla-api', { reqheaders: { 'app-key': authToken } })
+    .get('/auth/logout')
+    .reply(204);
+
+  const store = mockStore({ authToken });
+
+  store.dispatch( actions.logout() )
+    .then(() => {
+      t.deepEqual(store.getActions(), [
+        { type: 'LOGOUT', payload: undefined }
+      ]);
+
+      t.doesNotThrow(() => apiMock.done());
+      done();
+    })
+    .catch(done);
+
+});
+
+test('actions/logout access denied', t => {
+  const done = res => {
+    t.end(res);
+    nock.cleanAll();
+  };
+
+  const apiMock = nock('http://ndla-api', { reqheaders: { 'app-key': authToken } })
+    .get('/auth/logout')
+    .reply(403, {message: 'Invalid'});
+
+  const store = mockStore({ authToken });
+  store.dispatch( actions.logout() )
+    .then(() => {
+      t.deepEqual(store.getActions(), [
+        actions.applicationError(payload403invalid())
+      ]);
+
+      t.doesNotThrow(() => apiMock.done());
+      done();
+    })
+    .catch(done);
+});
