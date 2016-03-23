ce4ada4c12148a35a63797493c930ff48467a5f7
Kristofer Walters <kw@knowit.no>
2016-03-23 09:58:49 +0100

    Test to assure dismiss button dispatches action

--- src/components/__tests__/Alerts-test.js
+++ src/components/__tests__/Alerts-test.js
@@ -3,7 +3,9 @@ import React from 'react';
 import jsxAssertions from '@kwltrs/tape-jsx-assertions';
 import addAssertions from 'extend-tape';
 import { shallow } from 'enzyme';
+import sinon from 'sinon';
 import { Alerts } from '../Alerts';
+import { clearMessages } from '../../actions';
 
 const test = addAssertions(tape, jsxAssertions);
 
@@ -44,5 +46,19 @@ test('component/Alerts info and danger severity', t => {
 
   t.equals(listElements.length, 2);
   t.equals(dangerElement.html(), infoElement.html());
+  t.end();
+});
+
+test('component/Alerts dismiss', t => {
+  const dispatch = sinon.spy(() => {});
+
+  const dismissBt = shallow(<Alerts messages={[{message: 'whatever', severity: 'info'}]}
+      dispatch={dispatch} />).find('.alert_dismiss');
+
+  dismissBt.simulate('click');
+
+  t.ok(dispatch.calledOnce);
+  t.deepEquals(dispatch.firstCall.args, [ clearMessages() ]);
+
   t.end();
 });
