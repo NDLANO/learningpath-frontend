21433c1290eeda3abe116fbd7c2a40604cc29243
Henrik Nårstad <henrik.narstad@knowit.no>
2016-04-14 10:34:40 +0200

    NDLA-171: Enklere struktur på dropdown

--- src/components/LearningPathDropdown.js
+++ src/components/LearningPathDropdown.js
@@ -6,17 +6,13 @@ import Icon from './Icon';
 export function LearningPathDropdown ({dispatch, learningPath}) {
   return (
     <div className='dropdown-menu'>
-      <ul>
-        <li>
-          <button className='un-button'><Icon.MoreVert color="gray" /></button>
-          <ul className='dropdown-items'>
-            {(() => learningPath.status === 'PRIVATE'
-              ? <li className='dropdown-item'><a href="#" onClick={() => dispatch(updateLearningPathStatus(learningPath.id, 'PUBLISHED'))}><Icon.Input color="gray" /> Publiser</a></li>
-              : <li className='dropdown-item'><a href="#" onClick={() => dispatch(updateLearningPathStatus(learningPath.id, 'PRIVATE'))}><Icon.Input color="gray" /> De-publiser</a></li>
-            )()}
-            <li className='dropdown-item'><a href="#" onClick={() => dispatch(deleteLearningPath(learningPath.id))}><Icon.Delete color="gray" /> Slett</a></li>
-          </ul>
-        </li>
+      <span className='dropdown-icon'><Icon.MoreVert color="gray" /></span>
+      <ul className='dropdown-items'>
+        {(() => learningPath.status === 'PRIVATE'
+          ? <li className='dropdown-item'><a href="#" onClick={() => dispatch(updateLearningPathStatus(learningPath.id, 'PUBLISHED'))}><Icon.Input color="gray" /> Publiser</a></li>
+          : <li className='dropdown-item'><a href="#" onClick={() => dispatch(updateLearningPathStatus(learningPath.id, 'PRIVATE'))}><Icon.Input color="gray" /> De-publiser</a></li>
+        )()}
+        <li className='dropdown-item'><a href="#" onClick={() => dispatch(deleteLearningPath(learningPath.id))}><Icon.Delete color="gray" /> Slett</a></li>
       </ul>
     </div>
   );
