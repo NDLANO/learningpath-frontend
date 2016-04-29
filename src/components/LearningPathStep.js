ef27fdff7f293c134f967f3b259668ca8e733a6b
Christian Peter <chrpeter@Christians-MacBook-Pro-2.local>
2016-04-29 14:21:33 +0200

    Fikser kommentarer fra PR. NDLA-187

--- src/components/LearningPathStep.js
+++ src/components/LearningPathStep.js
@@ -19,47 +19,52 @@ let resizeIframe = (iframes) => {
     if (!iframe) { return; }
 
     if ((evt.data.height + 25) > parseInt(iframe.style.height, 10) || iframe.style.height === '') {
-      iframe.style.height = (evt.data.height + 25) + 'px';
+      iframe.style.height = (evt.data.height + 35) + 'px';
     }
   };
 };
 
-let resizeIframeFunc;
 
 export class LearningPathStep extends React.Component {
 
   checkIframe (props) {
+    if ( !ReactDOM.findDOMNode(this) ) {
+      return;
+    }
     let {step} = props;
     let {lang} = this.context;
     let url = oembedUrlI18N(step, lang);
 
     const ndlaIsSource = url != undefined ? ((/http:\/\/ndla.no/).test(url)) : false;
-    if (ReactDOM.findDOMNode(this) != null){
-      if(!this.resizeIframeFunc){
-        this.resizeIframeFunc = resizeIframe(ReactDOM.findDOMNode(this).children);
-      }
+    if(!this.resizeIframeFunc){
+      this.resizeIframeFunc = resizeIframe(ReactDOM.findDOMNode(this).children);
+    }
 
-      if (ndlaIsSource && ReactDOM.findDOMNode(this).children){
-        window.addEventListener('message', this.resizeIframeFunc);
-      }
-      else {
-        if (this.resizeIframeFunc != undefined){
-          window.removeEventListener('message', this.resizeIframeFunc);
-        }
-      }
+    if (ndlaIsSource) {
+      window.addEventListener('message', this.resizeIframeFunc);
+    }
+    else {
+      this.removeEventListener();
+    }
+
+  }
+
+  removeEventListener() {
+    if(this.resizeIframeFunc) {
+      window.removeEventListener('message', this.resizeIframeFunc);
     }
   }
 
   componentWillReceiveProps (props){
     this.checkIframe(props);
   }
+
   componentWillMount() {
     this.checkIframe(this.props);
   }
+
   componentWillUnmount () {
-    if(this.resizeIframeFunc != undefined){
-      window.removeEventListener('message', this.resizeIframeFunc);
-    }
+    this.removeEventListener();
   }
 
   render (){
