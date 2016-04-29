72f4470e7683adde239b07e1941d2c086f334d75
Rune Storløpa <rst@knowit.no>
2016-04-29 14:52:45 +0200

    NDLA-207: Navneendring i språkhåndteringen

--- src/components/EditLearningPathStep/index.js
+++ src/components/EditLearningPathStep/index.js
@@ -1,7 +1,7 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import assign from 'lodash/assign';
-import { titleI18N, descriptionI18N, embedUrlI18N } from '../../util/i18nFieldFinder';
+import { titleI18N, descriptionI18N, oembedUrlI18N } from '../../util/i18nFieldFinder';
 import LabeledIcon from '../LabeledIcon';
 import TitleEditor from '../editors/TitleEditor';
 import DescriptionHTMLEditor from '../editors/DescriptionHTMLEditor';
@@ -33,7 +33,7 @@ export function EditLearningPathStep (props, {lang}) {
   let saveLearningStep = () => saveAction(learningPathId, step);
   let title = titleI18N(step, lang) || '';
   let htmlDescription = descriptionI18N(step, lang) || '';
-  let embedContent = embedUrlI18N(step, lang);
+  let embedContent = oembedUrlI18N(step, lang);
 
   let embedSourceInput = '';
   if (step.type) {
