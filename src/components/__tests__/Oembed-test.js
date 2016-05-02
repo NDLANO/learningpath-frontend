c05a67c244a21ebfc3747418105526ed68584890
Kristofer Walters <kw@knowit.no>
2016-05-02 10:52:38 +0200

    NDLA-187 improve test

--- src/components/__tests__/Oembed-test.js
+++ src/components/__tests__/Oembed-test.js
@@ -8,15 +8,23 @@ import { ndlaLearningStep } from './mockData';
 import Oembed from '../Oembed';
 
 test('component/Oembed', t => {
-  let oembed = {url:learningStep.embedContent[0].url, html:learningStep.embedContent[0].html};
-  let ndlaOembed = {url:ndlaLearningStep.embedContent[0].url, html:ndlaLearningStep.embedContent[0].html};
+  const oembed = learningStep.embedContent[0];
   const component = shallow(<Oembed oembedContent={oembed} />);
-  const ndlaComponent = shallow(<Oembed oembedContent={ndlaOembed} />);
 
-  t.deepEqual(component.prop('dangerouslySetInnerHTML'), {__html: learningStep.embedContent[0].html});
-  t.deepEqual(ndlaComponent.prop('dangerouslySetInnerHTML'), {__html: ndlaLearningStep.embedContent[0].html});
-  t.equal(ndlaComponent.prop('className'), 'learning-step learning-step--without-dimensions');
-  t.equal(component.prop('className'), 'learning-step');
+  t.deepEqual(component.prop('dangerouslySetInnerHTML'), {__html: oembed.html});
+  t.ok(component.is('.learning-step'), 'has class .learning-step');
+  t.notOk(component.is('.learning-step--without-dimensions'), 'has not class .learning-step--without-dimensions');
+
+  t.end();
+});
+
+test('component/Oembed ndla resource', t => {
+  let oembed = ndlaLearningStep.embedContent[0];
+  const component = shallow(<Oembed oembedContent={oembed} />);
+
+  t.deepEqual(component.prop('dangerouslySetInnerHTML'), {__html: oembed.html});
+  t.ok(component.is('.learning-step'), 'has class .learning-step');
+  t.ok(component.is('.learning-step--without-dimensions'), 'has class .learning-step--without-dimensions');
 
   t.end();
 });
