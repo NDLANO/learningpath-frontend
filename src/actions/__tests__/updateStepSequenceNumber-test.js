2678330693d691c115be57eca08b850716bfbe6f
Christian Peter <chrpeter@Christians-MacBook-Pro-2.local>
2016-05-02 12:09:14 +0200

    Adds tests for update seq no NDLA-118

--- src/actions/__tests__/updateStepSequenceNumber-test.js
+++ src/actions/__tests__/updateStepSequenceNumber-test.js
@@ -6,7 +6,6 @@ import payload403invalid from './payload403invalid';
 import { isFSA } from 'flux-standard-action';
 
 import actions from '..';
-import { routerActions } from 'react-router-redux';
 
 const middleware = [ thunk ];
 const mockStore = configureStore(middleware);
@@ -17,73 +16,74 @@ const stepId = 321;
 const seqNo = 3;
 
 
-/*test('actions/updateStepSequenceNumber', t => {
+test('actions/updateStepSequenceNumber sucessfully', t => {
   const done = res => {
     t.end(res);
     nock.cleanAll();
   };
 
   const body = {
-    seqNo: 2
+    seqNo: 3
   };
 
 
 
   const learningStepReply = Object.assign({}, body, {});
 
-  console.log('LGLLLGLGLGLGLLGLGLGLGLLGLGLGLGLLGLGLGLLGLGLG');
+
   const updateSeqNo = nock('http://ndla-api', { reqheaders: { 'app-key': authToken } })
-    .put('/learningpaths/' + pathId + '/learningsteps/' + stepId + '/seqNo/', seqNo)
+    .put('/learningpaths/' + pathId + '/learningsteps/' + stepId + '/seqNo', body)
     .reply(200, learningStepReply);
-  console.log(updateSeqNo);
-  console.log('LGLLLGeeeeeeeeeeeeeeLGLGLGLLGLGLGLGLLGLGLGLGLLGLGLGLLGLGLG');
+  const fetchLearningPath = nock('http://ndla-api', { reqheaders: { 'app-key': authToken } })
+    .get('/learningpaths/' + pathId)
+    .reply(200, {id: pathId});
 
   const store = mockStore({ authToken });
 
   store.dispatch( actions.updateStepSequenceNumber(pathId, stepId, seqNo) )
     .then(() => {
-      console.log(store.getActions());
       t.deepEqual(store.getActions(), [
-        actions.updateLearningPathStepSeqNo(seqNo)
+        actions.setLearningPath({id: pathId})
       ]);
-
-      t.doesNotThrow(() => updateSeqNo.done());
-
-      done();
-    })
-    .catch(done);
-});*/
-
-/*test('actions/updateStepSequenceNumber access denied', (t) => {
-  const done = res => {
-    t.end(res);
-    nock.cleanAll();
-  };
-
-  const learningStep = {
-    title: [{language: 'nb', title: 'Goat'}],
-    description: [{language: 'nb', description: 'this is a description'}],
-    embedContent: [{language: 'nb', url: 'https://www.youtube.com/watch?v=ggB33d0BLcY'}]
-  };
-
-  const apiMock = nock('http://ndla-api', { reqheaders: { 'app-key': authToken } })
-    .put('/learningpaths/' + pathId + '/learningsteps/' + stepId, learningStep)
-    .reply(403, {message: 'Invalid'});
-
-  const store = mockStore({ authToken });
-
-  store.dispatch( actions.updateLearningPathStep(pathId, stepId, learningStep) )
-    .then(() => {
-      t.deepEqual(store.getActions(), [
-        actions.applicationError(payload403invalid())
-      ]);
-      t.doesNotThrow(() => apiMock.done());
+      t.doesNotThrow(() => nock.isDone());
 
       done();
     })
     .catch(done);
 });
-*/
+
+test('actions/updateStepSequenceNumber access denied', t => {
+  const done = res => {
+    t.end(res);
+    nock.cleanAll();
+  };
+
+  const body = {
+    seqNo: 3
+  };
+
+  const updateSeqNo = nock('http://ndla-api', { reqheaders: { 'app-key': authToken } })
+    .put('/learningpaths/' + pathId + '/learningsteps/' + stepId + '/seqNo', body)
+    .reply(403, {message: 'Invalid'});
+
+  const fetchLearningPath = nock('http://ndla-api', { reqheaders: { 'app-key': authToken } })
+    .get('/learningpaths/' + pathId)
+    .reply(200, {id: pathId});
+
+  const store = mockStore({ authToken });
+
+  store.dispatch( actions.updateStepSequenceNumber(pathId, stepId, seqNo) )
+    .then(() => {
+      t.deepEqual(store.getActions(), [
+        actions.applicationError(payload403invalid())
+      ]);
+      t.doesNotThrow(() => nock.isDone());
+
+      done();
+    })
+    .catch(done);
+});
+
 test('actions/sortSteps', t => {
   const actual = actions.updateLearningPathStepSeqNo({ title: 'whatever', language: 'eo' });
 
--- src/actions/updateStepSequenceNumber.js
+++ src/actions/updateStepSequenceNumber.js
@@ -1,8 +1,8 @@
-import { updateLearningPathStepSeqNo, applicationError } from '.';
+import { fetchLearningPath, applicationError } from '.';
 import { updateSeqNo } from '../sources/learningpaths';
 
 export default function updateStepSequenceNumber (pathId, stepId, seqNo) {
   return (dispatch, getState) => updateSeqNo(getState().authToken, {pathId, stepId}, {'seqNo': seqNo})
-    .then((object) => dispatch(updateLearningPathStepSeqNo(object)))
+    .then(() => dispatch(fetchLearningPath(pathId)))
     .catch(err => dispatch(applicationError(err)));
 }
