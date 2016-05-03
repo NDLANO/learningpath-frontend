41ad0fad4ce1f6935fc65aedc62229d5ddc087e2
Christian Peter <chrpeter@Christians-MacBook-Pro-2.local>
2016-05-03 10:44:23 +0200

    Fixes a small bug in navbar

--- src/components/LearningPath/LearningPathPrevNext.js
+++ src/components/LearningPath/LearningPathPrevNext.js
@@ -50,8 +50,9 @@ LearningPathPrevNext.contextTypes = {
 const mapStateToProps = (state) => {
   const learningsteps = get(state.learningPath, 'learningsteps', []);
 
-  const currentSeqNo = get(state.learningPathStep, 'seqNo', -1) - 1;
-
+  const currentSeqNo = get(state.learningPathStep, 'seqNo', -1);
+  console.log(learningsteps);
+  console.log(currentSeqNo)
   return Object.assign({}, state, {
     nextUrl: learningsteps[currentSeqNo + 1],
     prevUrl: learningsteps[currentSeqNo - 1],
