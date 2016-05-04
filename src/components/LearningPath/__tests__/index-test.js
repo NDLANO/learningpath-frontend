63e342fd1c0a3244b59c6a230cb539e72c6a98ef
Kristofer Walters <kw@knowit.no>
2016-05-04 11:28:54 +0200

    NDLA-226 spanderer på enhetstester

---
+++ src/components/LearningPath/__tests__/index-test.js
@@ -0,0 +1,35 @@
+import test from 'tape';
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import { learningPath } from '../../__tests__/mockData';
+import { LearningPath } from '..';
+
+const FooBar = () => <div></div>;
+
+test('component/LearningPath', t => {
+  t.equal(
+    shallow(
+      <LearningPath learningPath={learningPath}>
+        <FooBar />
+      </LearningPath>
+    ).find(FooBar).length, 1, 'renders props.children');
+
+  t.equal(
+    shallow(
+      <LearningPath
+        learningPath={learningPath}
+        saveButtons={React.createElement(FooBar)}
+      />
+    ).find(FooBar).length, 1, 'renders props.saveButtons');
+
+  t.equal(
+    shallow(
+      <LearningPath
+        learningPath={learningPath}
+        main={React.createElement(FooBar)}
+      />
+    ).find(FooBar).length, 1, 'renders props.main');
+
+  t.end();
+});
