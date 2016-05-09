1d5c2c24b5f75b0b9b347f753220e1d21e4b3802
Christian Peter <christian.peter@knowit.no>
2016-05-09 11:50:56 +0200

    Removes e from pathide NDLA-173

--- src/actions/deletePersistedLearningPathStep.js
+++ src/actions/deletePersistedLearningPathStep.js
@@ -2,9 +2,9 @@ import { fetchLearningPath } from '.';
 import { deleteStep } from '../sources/learningpaths';
 import { routerActions } from 'react-router-redux';
 
-export default function deletePersistedLearningPathStep(pathIde, learningPathStep) {
-  return (dispatch, getState) => deleteStep(getState().authToken, {pathId: pathIde, stepId: learningPathStep.id})
-    .then(() => dispatch(fetchLearningPath(pathIde)))
+export default function deletePersistedLearningPathStep(pathId, learningPathStep) {
+  return (dispatch, getState) => deleteStep(getState().authToken, {pathId: pathId, stepId: learningPathStep.id})
+    .then(() => dispatch(fetchLearningPath(pathId)))
     .then(() => dispatch(routerActions.push({
-      pathname: `/learningpaths/${pathIde}`})));
+      pathname: `/learningpaths/${pathId}`})));
 }
