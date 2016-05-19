8173831e08fa823c6e6563f4171ebcec800cb4b0
Christian Peter <christian.peter@knowit.no>
2016-05-19 08:24:44 +0200

    Removes ugly picture if it is a image error NDLA-248

--- src/components/LearningPathSearch.js
+++ src/components/LearningPathSearch.js
@@ -82,31 +82,57 @@ SearchForm.defaultProps = {
   sort: '-lastUpdated', query: ''
 };
 
-function SearchResult ({path}, {lang}) {
-  return (
-    <div>
-      <Link to={`/learningpaths/${path.id}`}>
-        <div className='search-result'>
-          <div className='search-result_img_container'>
-            {(() => path.coverPhotoUrl ?
-              (<img className='search-result_img' src={path.coverPhotoUrl} />) : (<img className='search-result_img' src={'https://placeholdit.imgix.net/~text?txtsize=33&txt=NDLA&w=190&h=120'} />)
-            )()}
-          </div>
-          <div className='search-result_bd'>
-            <h2 className='search-result_title'>
-              {titleI18N(path, lang)}
-            </h2>
-            <div className='search-result_meta'>
-              <LabeledIcon.Person labelText={get(path, 'author.name')} />
-              <LabeledIcon.Today labelText={formatDate(path.lastUpdated, lang)} tagName='time' />
-              <LabeledIcon.QueryBuilder labelText={formatDuration(path.duration, lang)} tagName='time' />
+
+
+export class SearchResult extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {imageError: false};
+    this.handleImageError = this.handleImageError.bind(this);
+  }
+
+  handleImageError () {
+    this.setState({imageError: true});
+  }
+
+  render () {
+
+    const { path, errored } = this.props;
+    const { lang } = this.context;
+    const image = () => {
+
+      if (path.coverPhotoUrl && !this.state.imageError){
+        return <img className='search-result_img' src={path.coverPhotoUrl} onError={this.handleImageError}/>;
+      }
+      else {
+        return <img className='search-result_img' src={'https://placeholdit.imgix.net/~text?txtsize=33&txt=NDLA&w=190&h=120'} />;
+      }
+    }
+
+    return (
+
+      <div>
+        <Link to={`/learningpaths/${path.id}`}>
+          <div className='search-result'>
+            <div className='search-result_img_container'>
+              {image()}
             </div>
-            <div className='search-result_description'>{descriptionI18N(path, lang)}</div>
+            <div className='search-result_bd'>
+              <h2 className='search-result_title'>
+                {titleI18N(path, lang)}
+              </h2>
+              <div className='search-result_meta'>
+                <LabeledIcon.Person labelText={get(path, 'author.name')} />
+                <LabeledIcon.Today labelText={formatDate(path.lastUpdated, lang)} tagName='time' />
+                <LabeledIcon.QueryBuilder labelText={formatDuration(path.duration, lang)} tagName='time' />
+              </div>
+              <div className='search-result_description'>{descriptionI18N(path, lang)}</div>
+            </div>
           </div>
-        </div>
-      </Link>
-    </div>
-  );
+        </Link>
+      </div>
+    );
+  }
 }
 
 SearchResult.propTypes = {
@@ -119,6 +145,7 @@ SearchResult.contextTypes = {
 
 export class LearningPathSearch extends Component {
 
+
   componentWillMount () {
     this.props.fetchLearningPaths();
   }
@@ -128,6 +155,7 @@ export class LearningPathSearch extends Component {
       newProps.fetchLearningPaths();
     }
   }
+
 
   render () {
     const { learningPaths, query, lastPage, location: { pathname }, pushRoute } = this.props;
@@ -152,7 +180,7 @@ export class LearningPathSearch extends Component {
 
         <div className='search-results'>
           {learningPaths.map(path =>
-            (<SearchResult key={path.id} path={path} />)
+            (<SearchResult key={path.id} path={path}/>)
           )}
           <SearchResultPager page={page} lastPage={lastPage} query={query} />
         </div>
