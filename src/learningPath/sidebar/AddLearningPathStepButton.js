71f33d7e8a6a50ddae934f197382e8e8ea1f16a5
Christian Peter <christian.peter@knowit.no>
2016-06-10 15:30:46 +0200

    No longer uses vertical menu for add step button NDLA-298

--- src/learningPath/sidebar/AddLearningPathStepButton.js
+++ src/learningPath/sidebar/AddLearningPathStepButton.js
@@ -13,16 +13,12 @@ export function AddLearningPathStepButton({learningPath, localCloseSidebars}) {
   const newStepTarget = `/learningpaths/${learningPath.id}/step/new`;
 
   return (
-    <div>
+    <div className="add-learningpath-step">
       <div className="add-learningpath-step_line" />
-      <ul className="vertical-menu">
-        <li className="vertical-menu_item">
-          <Link to={newStepTarget} className="add-learningpath-step_button cta-link cta-link--block cta-link--secondary" onClick={localCloseSidebars}>
-            <div className="plus-sign--circle">+</div>
-            {polyglot.t('editPage.addStepBtn')}
-          </Link>
-        </li>
-      </ul>
+      <Link to={newStepTarget} className="add-learningpath-step_button cta-link cta-link--block cta-link--secondary" onClick={localCloseSidebars}>
+        <div className="plus-sign--circle">+</div>
+        {polyglot.t('editPage.addStepBtn')}
+      </Link>
     </div>
   );
 }
