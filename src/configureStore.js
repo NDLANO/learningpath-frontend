5e4cfa976fc5280d9d7ca8b3ff7ce9e394d15269
Øyvind Marthinsen <oma@knowit.no>
2016-06-29 08:02:38 +0200

    NDLA-337 Add locale as basename in router
    * Defaults to NB
    * Reloads page on locale change
    * Root path should be NB

--- src/configureStore.js
+++ src/configureStore.js
@@ -6,7 +6,6 @@ import reducers from './reducers';
 import { errorReporter, searchQueryMiddleware } from './middleware';
 
 import { routerMiddleware } from 'react-router-redux';
-import { browserHistory } from 'react-router';
 
 
 const slicer = (paths) =>
@@ -17,19 +16,19 @@ const slicer = (paths) =>
     return acc;
   }, {});
 
-const middleware = routerMiddleware(browserHistory);
-const createFinalStore = compose(
-  applyMiddleware(
-    thunkMiddleware,
-    searchQueryMiddleware,
-    errorReporter,
-    middleware
-  ),
-  persistState(['authenticated', 'authToken', 'user', 'locale'], { key: 'ndla:sti', slicer }),
-  window && window.devToolsExtension ? window.devToolsExtension() : f => f
-)(createStore);
+export default function configureStore(initialState, history) {
+  const middleware = routerMiddleware(history);
 
+  const createFinalStore = compose(
+    applyMiddleware(
+      thunkMiddleware,
+      searchQueryMiddleware,
+      errorReporter,
+      middleware
+    ),
+    persistState(['authenticated', 'authToken', 'user'], { key: 'ndla:sti', slicer }),
+    window && window.devToolsExtension ? window.devToolsExtension() : f => f
+  )(createStore);
 
-export default function configureStore(initialState) {
   return createFinalStore(reducers, initialState);
 }
--- src/locale/SelectLocale.jsx
+++ src/locale/SelectLocale.jsx
@@ -1,23 +1,37 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
+import { createHistory } from 'history';
 
-import { setLocale } from './localeActions';
+import { availableLocales } from './localeConstants';
 import { getLocale } from './localeSelectors';
 
-const SelectLocale = ({ locale, changeLocale }) =>
-  <select onChange={(evt) => { changeLocale(evt.target.value); }} value={locale}>
-    <option value="en">English</option>
-    <option value="nb">Norsk</option>
-  </select>
+const SelectLocale = ({ locale, pathname, search }) => {
+  const handleChange = (newLocale) => {
+    const path = pathname.startsWith('/') ? pathname.substring(1) : pathname;
+    createHistory().push(`/${newLocale}/${path}${search}`); // Need create new history or else basename is included
+    window.location.reload();
+  };
+
+  return (
+    <select onChange={(evt) => { handleChange(evt.target.value); }} value={locale}>
+      {availableLocales.map(l => <option key={l.abbreviation} value={l.abbreviation}>{l.name}</option>)}
+    </select>
+  );
+}
 ;
 
 SelectLocale.propTypes = {
   locale: PropTypes.string.isRequired,
-  changeLocale: PropTypes.func.isRequired,
+  pathname: PropTypes.string.isRequired,
+  search: PropTypes.string.isRequired,
 };
 
-const mapDispatchToProps = { changeLocale: setLocale };
+const mapStateToProps = (state) => Object.assign(
+  {}, state, {
+    locale: getLocale(state),
+    pathname: state.routing.locationBeforeTransitions.pathname,
+    search: state.routing.locationBeforeTransitions.search
+  }
+);
 
-const mapStateToProps = (state) => Object.assign({}, state, { locale: getLocale(state) });
-
-export default connect(mapStateToProps, mapDispatchToProps)(SelectLocale);
+export default connect(mapStateToProps)(SelectLocale);
