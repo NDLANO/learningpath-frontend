239cbd8d93ee369d438d87d8bdd41d7606036f2e
Christian Peter <christian.peter@knowit.no>
2017-05-08 14:41:49 +0200

    Fixes missing window ref

--- src/util/imageScaler.js
+++ src/util/imageScaler.js
@@ -1,4 +1,7 @@
 export function scaleImage(imageUrl) {
+  if (__SERVER__) {
+    return 400;
+  }
   const newImageWidth = window.innerWidth > 400 ? window.innerWidth / 4 : window.innerWidth;
   return `${imageUrl}?width=${newImageWidth}`;
 }
