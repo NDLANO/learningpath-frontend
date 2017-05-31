b761d5cfbfffa5d72f5e14b7bf8545031c3334f8
Christian Peter <chrpeter@Christians-MacBook-Pro-2.local>
2017-05-31 13:30:59 +0200

    Removes message as it is not used #373

--- src/session/LoginProviders.js
+++ src/session/LoginProviders.js
@@ -7,23 +7,17 @@
  */
 
 import React from 'react';
-import PropTypes from 'prop-types';
 import polyglot from '../i18n';
 import { loginSocialMedia } from './sessionActions';
 
-const LoginProviders = ({ message }) => (
+const LoginProviders = () => (
   <div className="one-column one-column--narrow">
     <h3>{polyglot.t('loginProviders.description')}</h3>
-    {message ? <p>{message}</p> : null}
     <ul className="vertical-menu">
       <li className="vertical-menu_item"><button onClick={() => loginSocialMedia('google-oauth2')} className="un-button cta-link cta-link--block cta-link--gl">Google</button></li>
       <li className="vertical-menu_item"><button onClick={() => loginSocialMedia('facebook')} className="un-button cta-link cta-link--block cta-link--fb">Facebook</button></li>
     </ul>
   </div>
   );
-
-LoginProviders.propTypes = {
-  message: PropTypes.string,
-};
 
 export default LoginProviders;
