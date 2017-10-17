d625c683ad5f97050606c58371c48bd113925e64
Christian Peter <christian150790@gmail.com>
2017-10-17 14:00:34 +0200

    Fixes breaking change with nested tests. See changelog for jest 21.0.0 #589

--- src/common/__tests__/TestSetup-test.js
+++ src/common/__tests__/TestSetup-test.js
@@ -28,23 +28,20 @@ class Selfie extends Component {
 Selfie.propTypes = { foo: PropTypes.string };
 Selfie.defaultProps = { foo: 'default bar' };
 
+test('components/TestSetup selftest - unit test mocking', () => {
+  expect(locationOrigin).toBe('http://ndla-frontend');
+  expect(apiBaseUrl).toBe('http://ndla-api');
+  expect(accessToken).toBe('ndlatestapikey');
+});
 
-test('components/TestSetup selftest', () => {
-  test('- unit test mocking', () => {
-    expect(locationOrigin).toBe('http://ndla-frontend');
-    expect(apiBaseUrl).toBe('http://ndla-api');
-    expect(accessToken).toBe('ndlatestapikey');
-  });
+test('components/TestSetup selftest - Enzyme shallow rendering', () => {
+  expect(() => shallow(<Selfie />)).not.toThrow();
+});
 
-  test('- Enzyme shallow rendering', () => {
-    expect(() => shallow(<Selfie />)).not.toThrow();
-  });
+test('components/TestSetup selftest - Enzyme full dom rendering', () => {
+  expect(() => mount(<Selfie />)).not.toThrow();
+});
 
-  test('- Enzyme full dom rendering', () => {
-    expect(() => mount(<Selfie />)).not.toThrow();
-  });
-
-  test('- Enzyme static rendering', () => {
-    expect(() => render(<Selfie />)).not.toThrow();
-  });
+test('components/TestSetup selftest - Enzyme static rendering', () => {
+  expect(() => render(<Selfie />)).not.toThrow();
 });
