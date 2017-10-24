3837753561db0c9b031362bac4eefa208ff6ca95
Christian Peter <christian150790@gmail.com>
2017-10-24 13:10:44 +0200

    Fixes saving of description and embed urls. Also fixes pinterest import

--- src/util/formatFormFieldsUtil.js
+++ src/util/formatFormFieldsUtil.js
@@ -1,12 +1,14 @@
 
-export function formattedEmbedUrl(url) {
-  return url && url.url ? { url: url.url, embedType: url.embedType } : undefined
+export function formattedEmbedUrl(step, url) {
+  const defaultValue = step.id ? { url: '', embedType: 'oembed' } : undefined;
+  return url && url.url ? { url: url.url, embedType: url.embedType } : defaultValue;
 }
 
 export function formattedEmbedLicense(license) {
   return license && license.license ? license.license : '';
 }
 
-export function formattedEmbedDescription(description) {
-  return description || '';
+export function formattedEmbedDescription(step, description) {
+  const defaultValue = step.id ? '' : undefined;
+  return description || defaultValue;
 }
