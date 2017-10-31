a8cceee91127c677e860da1c8a8167ef7d8e35b6
Christian Peter <christian150790@gmail.com>
2017-10-31 10:33:13 +0100

    Fixes require assets

--- src/util/requireAssets.js
+++ src/util/requireAssets.js
@@ -1,7 +1,5 @@
 import config from '../config';
 
-const requireAssets = __CLIENT__ ? window.assets : ( // eslint-disable-line no-nested-ternary
-  config.isProduction ? require('../../htdocs/assets/assets') : require('../../server/developmentAssets') // eslint-disable-line import/no-unresolved
-);
+const requireAssets = config.isProduction ? require('../../htdocs/assets/assets') : require('../../server/developmentAssets');
 
 export default requireAssets;
