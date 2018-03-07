0ee42912628f5e7be6bf4d8188e9b5cc778b4dd6
Christian Peter <christian150790@gmail.com>
2018-03-07 13:14:23 +0100

    Adds fix for widget reference error

--- server/ZendeskWidget.js
+++ server/ZendeskWidget.js
@@ -11,6 +11,13 @@ import PropTypes from 'prop-types';
 
 const ZendeskWidget = ({ lang }) => [
   <script
+    key="zendesk-widget"
+    dangerouslySetInnerHTML={{
+      __html: `/*<![CDATA[*/window.zEmbed||function(e,t){var n,o,d,i,s,a=[],r=document.createElement("iframe");window.zEmbed=function(){a.push(arguments)},window.zE=window.zE||window.zEmbed,r.src="javascript:false",r.title="",r.role="presentation",(r.frameElement||r).style.cssText="display: none",d=document.getElementsByTagName("script"),d=d[d.length-1],d.parentNode.insertBefore(r,d),i=r.contentWindow,s=i.document;try{o=s}catch(e){n=document.domain,r.src='javascript:var d=document.open();d.domain="'+n+'";void(0);',o=s}o.open()._l=function(){var e=this.createElement("script");n&&(this.domain=n),e.id="js-iframe-async",e.src="https://assets.zendesk.com/embeddable_framework/main.js",this.t=+new Date,this.zendeskHost="ndla.zendesk.com",this.zEQueue=a,this.body.appendChild(e)},o.write('<body onload="document._l();">'),o.close()}();
+  /*]]>*/`,
+    }}
+  />,
+  <script
     key="zendesk-config"
     dangerouslySetInnerHTML={{
       __html: `
@@ -18,13 +25,6 @@ const ZendeskWidget = ({ lang }) => [
           zE.setLocale('${lang}');
           zE.hide();
         });`,
-    }}
-  />,
-  <script
-    key="zendesk-widget"
-    dangerouslySetInnerHTML={{
-      __html: `/*<![CDATA[*/window.zEmbed||function(e,t){var n,o,d,i,s,a=[],r=document.createElement("iframe");window.zEmbed=function(){a.push(arguments)},window.zE=window.zE||window.zEmbed,r.src="javascript:false",r.title="",r.role="presentation",(r.frameElement||r).style.cssText="display: none",d=document.getElementsByTagName("script"),d=d[d.length-1],d.parentNode.insertBefore(r,d),i=r.contentWindow,s=i.document;try{o=s}catch(e){n=document.domain,r.src='javascript:var d=document.open();d.domain="'+n+'";void(0);',o=s}o.open()._l=function(){var e=this.createElement("script");n&&(this.domain=n),e.id="js-iframe-async",e.src="https://assets.zendesk.com/embeddable_framework/main.js",this.t=+new Date,this.zendeskHost="ndla.zendesk.com",this.zEQueue=a,this.body.appendChild(e)},o.write('<body onload="document._l();">'),o.close()}();
-  /*]]>*/`,
     }}
   />,
 ];
