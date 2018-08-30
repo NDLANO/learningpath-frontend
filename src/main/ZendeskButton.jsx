2bac4ebe9cd77c4dafb3d70beb5cbe34bc16f806
Christian Peter <christian150790@gmail.com>
2018-08-30 18:33:30 +0200

    Fixes zendesk button. Moves up so it does not block other buttons. Fixes masthead fontsize #1215

--- src/main/ZendeskButton.jsx
+++ src/main/ZendeskButton.jsx
@@ -7,19 +7,67 @@
  */
 
 import React from 'react';
+import classNames from 'classnames';
+import { withRouter } from 'react-router-dom';
 import polyglot from '../i18n';
 import config from '../config';
 
 const { zendeskHost } = config;
 
-const ZendeskButton = () =>
-  zendeskHost ? (
-    <button
-      type="button"
-      onClick={() => (window && window.zE ? window.zE.activate() : undefined)}
-      className="c-zendesk__button">
-      {polyglot.t('askNDLA')}
-    </button>
-  ) : null;
+class ZendeskButton extends React.Component {
+  constructor() {
+    super();
+    this.state = { lastScrollPos: 0, isOpen: false, location: null };
 
-export default ZendeskButton;
+    this.handleScroll = this.handleScroll.bind(this);
+  }
+
+  componentDidMount() {
+    document.addEventListener('scroll', this.handleScroll);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('scroll', this.handleScroll);
+  }
+
+  static getDerivedStateFromProps(nextProps, prevState) {
+    if (prevState.location === null) {
+      return {
+        location: nextProps.location,
+        isOpen: false,
+      };
+    }
+    const { location } = nextProps;
+    const navigated = location !== prevState.location;
+    if (navigated) {
+      return { isOpen: false, location };
+    }
+    return null;
+  }
+
+  handleScroll() {
+    this.setState(prevState => ({
+      isOpen: prevState.lastScrollPos > window.scrollY,
+      lastScrollPos: window.scrollY,
+    }));
+  }
+
+  render() {
+    const zendeskClassNames = classNames({
+      'c-zendesk__button': true,
+      'c-zendesk__button--open': this.state.isOpen,
+      'c-zendesk__button--hidden': !this.state.isOpen,
+    });
+
+    return zendeskHost ? (
+      <button
+        type="button"
+        onClick={() => (window && window.zE ? window.zE.activate() : undefined)}
+        className={zendeskClassNames}>
+        {polyglot.t('askNDLA')}
+      </button>
+    ) : null;
+  }
+}
+
+export default withRouter(ZendeskButton);
