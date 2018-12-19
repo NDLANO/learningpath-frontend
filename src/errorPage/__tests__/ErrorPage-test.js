51c99d1dec4442ab6fea52e945bf83ba0c1cf457
Øyvind Marthinsen <oma@knowit.no>
2018-12-19 15:33:42 +0100

    test: fix errorpage test

--- src/errorPage/__tests__/ErrorPage-test.js
+++ src/errorPage/__tests__/ErrorPage-test.js
@@ -13,6 +13,10 @@ import renderer from 'react-test-renderer';
 import ErrorPage from '../ErrorPage';
 import configureStore from '../../configureStore';
 
+jest.mock('../../config', () => ({
+  zendeskWidgetKey: '123',
+}));
+
 test('ErrorPage renderers correctly', () => {
   const store = configureStore({ locale: 'nb' });
   const component = renderer.create(
