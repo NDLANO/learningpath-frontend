259b11192e65249b1f890a6d31d295185c8bdb56
Jonas Natten <jonas.natten@knowit.no>
2020-01-31 12:30:18 +0100

    #1952: Parse ndla-frontend urls for new domains as well

--- src/learningPath/step/oembed/__tests__/urlIsNDLA-test.js
+++ src/learningPath/step/oembed/__tests__/urlIsNDLA-test.js
@@ -1,9 +1,19 @@
-import { urlIsProductionNDLA } from '../Oembed';
+import { urlIsNDLAUrl } from '../Oembed';
 
 test('component/Oembed urlIsNDLA', () => {
-  expect(urlIsProductionNDLA('http://ndla.no/nb/node/12345')).toBeTruthy();
+  expect(urlIsNDLAUrl('http://ndla.no/nb/node/12345')).toBeTruthy();
+  expect(urlIsNDLAUrl('http://www.ndla.no/nb/node/12345')).toBeTruthy();
   expect(
-    urlIsProductionNDLA('http://exampe.com/ndla.no/nb/node/12345'),
-  ).toBeFalsy();
-  expect(urlIsProductionNDLA()).toBeFalsy();
+    urlIsNDLAUrl('http://ndla-frontend.api.ndla.no/nb/node/12345'),
+  ).toBeTruthy();
+
+  expect(urlIsNDLAUrl('http://test.ndla.no/nb/node/12345')).toBeTruthy();
+  expect(urlIsNDLAUrl('http://www.test.ndla.no/nb/node/12345')).toBeTruthy();
+  expect(
+    urlIsNDLAUrl('http://ndla-frontend.test.api.ndla.no/nb/node/12345'),
+  ).toBeTruthy();
+
+  expect(urlIsNDLAUrl('http://exampe.com/ndla.no/nb/node/12345')).toBeFalsy();
+
+  expect(urlIsNDLAUrl()).toBeFalsy();
 });
