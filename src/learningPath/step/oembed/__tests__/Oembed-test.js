b00c177287388a433dece16211afef676b94d358
Jonas Ege Carlsen <jonas.carlsen@knowit.no>
2023-04-14 14:54:02 +0200

    Remove redundant tests

--- src/learningPath/step/oembed/__tests__/Oembed-test.js
+++ src/learningPath/step/oembed/__tests__/Oembed-test.js
@@ -20,19 +20,8 @@ import {
 import Oembed from '../Oembed';
 
 let component;
-let add;
-let remove;
-
-beforeEach(() => {
-  add = jest.spyOn(window, 'addEventListener');
-  remove = jest.spyOn(window, 'removeEventListener');
-});
 
 afterEach(() => {
-  add.mockReset();
-  add.mockRestore();
-  remove.mockReset();
-  remove.mockRestore();
   component.unmount();
 });
 
@@ -47,7 +36,6 @@ test('component/Oembed', () => {
   expect(div.prop('dangerouslySetInnerHTML')).toEqual({ __html: oembed.html });
 
   expect(component.state('isNDLAResource')).toBeFalsy();
-  expect(component.state('listeningToMessages')).toBeFalsy();
 });
 
 test('component/Oembed ndla resource', () => {
@@ -61,128 +49,4 @@ test('component/Oembed ndla resource', () => {
   expect(div.prop('dangerouslySetInnerHTML')).toEqual({ __html: oembed.html });
 
   expect(component.state('isNDLAResource')).toBeTruthy();
-  expect(component.state('listeningToMessages')).toBeTruthy();
-  // expect(add).toHaveBeenCalledTimes(1);
-  expect(add).toHaveBeenCalledWith(
-    'message',
-    component.instance().handleIframeMessages,
-  );
-});
-
-test('component/Oembed resize message listener', () => {
-  const { oembed } = translatedLearningStep;
-  const ndlaOembed1 = translatedNdlaLearningStep.oembed;
-  const ndlaOembed2 = Object.assign({}, ndlaOembed1, {
-    url: `${ndlaOembed1.url}?another`,
-  });
-  const ndlaOembed3 = Object.assign({}, ndlaOembed1, {
-    url: `${ndlaOembed1.url}?yetanother`,
-  });
-
-  // initialize with ndla resource
-  component = mount(<Oembed oembedContent={ndlaOembed1} />);
-
-  // expect(add).toHaveBeenCalledTimes(1);
-  // expect(remove).toHaveBeenCalledTimes(0);
-
-  // update with other ndla resource
-  component.setProps({ oembedContent: ndlaOembed2 });
-
-  // expect(add).toHaveBeenCalledTimes(1);
-  // expect(remove).toHaveBeenCalledTimes(0);
-
-  // update with non-ndla resource
-  component.setProps({ oembedContent: oembed });
-
-  // expect(add).toHaveBeenCalledTimes(1);
-  // expect(remove).toHaveBeenCalledTimes(1);
-
-  expect(component.state('isNDLAResource')).toBeFalsy();
-  expect(component.state('listeningToIframeMessages')).toBeFalsy();
-
-  expect(remove).toHaveBeenCalledWith(
-    'message',
-    component.instance().handleIframeMessages,
-  );
-
-  // update with other ndla resource
-  component.setProps({ oembedContent: ndlaOembed3 });
-});
-
-test('component/Oembed iframe resizing', () => {
-  const ndlaOembed = translatedNdlaLearningStep.oembed;
-  const { oembed } = translatedLearningStep;
-
-  component = mount(<Oembed oembedContent={ndlaOembed} />);
-  const instance = component.instance();
-
-  const iframe = instance.getIframeDOM();
-
-  expect(iframe).toBeTruthy();
-  expect(iframe.src).toBe('http://ndla.no/nb/node/24049/oembed');
-
-  instance.handleIframeMessages({
-    source: iframe.contentWindow,
-    data: {
-      height: '800',
-      event: 'resize',
-    },
-  });
-
-  expect(iframe.style.height).toBe('800px');
-
-  instance.handleIframeMessages({
-    source: iframe.contentWindow,
-    data: { height: '1000', event: 'resize' },
-  });
-
-  expect(iframe.style.height).toBe('1000px');
-
-  instance.handleIframeMessages({
-    source: iframe.contentWindow,
-    data: { height: '900', event: 'resize' },
-  });
-
-  expect(iframe.style.height).toBe('900px');
-
-  expect(() => instance.handleIframeMessages()).not.toThrow();
-
-  instance.handleIframeMessages({
-    source: { mysteryObject: true },
-    data: { height: '2000', event: 'resize' },
-  });
-
-  expect(iframe.style.height).toBe('900px');
-
-  component.setProps({ oembedContent: oembed });
-
-  const nextIframe = instance.getIframeDOM();
-  expect(nextIframe.style.height).toBe('');
-
-  instance.handleIframeMessages({
-    source: nextIframe.contentWindow,
-    data: { height: '2000', event: 'resize' },
-  });
-
-  expect(nextIframe.style.height).toBe('');
-});
-
-test('component/Oembed iframe scrollTo message', () => {
-  const ndlaOembed = translatedNdlaLearningStep.oembed;
-
-  component = mount(<Oembed oembedContent={ndlaOembed} />);
-  const instance = component.instance();
-  const spy = jest.spyOn(instance, 'handleIframeMessages');
-  const iframe = instance.getIframeDOM();
-
-  instance.handleIframeMessages({
-    source: iframe.contentWindow,
-    data: {
-      height: '800',
-      event: 'scrollTo',
-      top: 3000,
-    },
-  });
-
-  expect(spy).toHaveBeenCalledTimes(1);
 });
